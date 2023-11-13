506993403ce3e555106db95802eded6172cf08d5
Mo Xiaoming <2188767+mo-xiaoming@users.noreply.github.com>
2023-11-13 10:57:13 +0800

    remove some snap installs for theirs unreliablility

--- init.sh
+++ init.sh
@@ -4,18 +4,18 @@ set -euo pipefail
 
 CFGDIR=~/repo/ubuntu-setup
 
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+
 manual_install() {
   # browser downloads chrome
 }
 
 install_system_packages() {
-  sudo snap install --classic code nvim \
-    # curl # snap curl has some permission problem
-  sudo snap install yt-dlp htop aria2c
   sudo apt install -y g++ git python3-pip tmux mpv curl direnv openssh-server pkg-config cmake-format valgrind \
-    sysstat     # for tmux-cpu
-    nodejs npm  # for coc.vim
-    silversearcher-ag # for ctrlp
+    sysstat     \ # for tmux-cpu
+    nodejs npm  \ # for coc.vim
+    silversearcher-ag  \ # for ctrlp
+    yt-dlp htop aria2c nvim
   sudo pip3 install --user --upgrade conan ninja cmake
 }
 
@@ -50,38 +50,38 @@ install_cpp_env() {
 }
 
 install_bash_env() {
-  # git prompt
-  git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
-
-  # .bashrc
-  cat <<EOF>>~/.bashrc
-
-  eval $(direnv hook bash)
-
-  if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
-    GIT_PROMPT_ONLY_IN_REPO=1
-
-    # GIT_PROMPT_FETCH_REMOTE_STATUS=0   # uncomment to avoid fetching remote status
-    # GIT_PROMPT_IGNORE_SUBMODULES=1 # uncomment to avoid searching for changed files in submodules
-    # GIT_PROMPT_WITH_VIRTUAL_ENV=0 # uncomment to avoid setting virtual environment infos for node/python/conda environments
-
-    GIT_PROMPT_SHOW_UPSTREAM=1 # uncomment to show upstream tracking branch
-    GIT_PROMPT_SHOW_UNTRACKED_FILES=normal # can be no, normal or all; determines counting of untracked files
-
-    # GIT_PROMPT_SHOW_CHANGED_FILES_COUNT=0 # uncomment to avoid printing the number of changed files
-
-    # GIT_PROMPT_STATUS_COMMAND=gitstatus_pre-1.7.10.sh # uncomment to support Git older than 1.7.10
-
-    # GIT_PROMPT_START=...    # uncomment for custom prompt start sequence
-    # GIT_PROMPT_END=...      # uncomment for custom prompt end sequence
-
-    # as last entry source the gitprompt script
-    # GIT_PROMPT_THEME=Custom # use custom theme specified in file GIT_PROMPT_THEME_FILE (default ~/.git-prompt-colors.sh)
-    # GIT_PROMPT_THEME_FILE=~/.git-prompt-colors.sh
-    # GIT_PROMPT_THEME=Solarized # use theme optimized for solarized color scheme
-    source $HOME/.bash-git-prompt/gitprompt.sh
-  fi
-EOF
+#  # git prompt
+#  git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+#
+#  # .bashrc
+#  cat <<EOF>>~/.bashrc
+#
+#  eval $(direnv hook bash)
+#
+#  if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
+#    GIT_PROMPT_ONLY_IN_REPO=1
+#
+#    # GIT_PROMPT_FETCH_REMOTE_STATUS=0   # uncomment to avoid fetching remote status
+#    # GIT_PROMPT_IGNORE_SUBMODULES=1 # uncomment to avoid searching for changed files in submodules
+#    # GIT_PROMPT_WITH_VIRTUAL_ENV=0 # uncomment to avoid setting virtual environment infos for node/python/conda environments
+#
+#    GIT_PROMPT_SHOW_UPSTREAM=1 # uncomment to show upstream tracking branch
+#    GIT_PROMPT_SHOW_UNTRACKED_FILES=normal # can be no, normal or all; determines counting of untracked files
+#
+#    # GIT_PROMPT_SHOW_CHANGED_FILES_COUNT=0 # uncomment to avoid printing the number of changed files
+#
+#    # GIT_PROMPT_STATUS_COMMAND=gitstatus_pre-1.7.10.sh # uncomment to support Git older than 1.7.10
+#
+#    # GIT_PROMPT_START=...    # uncomment for custom prompt start sequence
+#    # GIT_PROMPT_END=...      # uncomment for custom prompt end sequence
+#
+#    # as last entry source the gitprompt script
+#    # GIT_PROMPT_THEME=Custom # use custom theme specified in file GIT_PROMPT_THEME_FILE (default ~/.git-prompt-colors.sh)
+#    # GIT_PROMPT_THEME_FILE=~/.git-prompt-colors.sh
+#    # GIT_PROMPT_THEME=Solarized # use theme optimized for solarized color scheme
+#    source $HOME/.bash-git-prompt/gitprompt.sh
+#  fi
+#EOF
 
   # .profile
   cat <<EOF>>~/.profile
@@ -94,7 +94,7 @@ EOF
 
   export GPG_TTY=$(tty)
 
-  export EDITOR=/snap/bin/nvim
+  export EDITOR=nvim
 
   export VCPKG_ROOT=$HOME/repos/toolchains/vcpkg
 EOF
@@ -125,6 +125,7 @@ install_nvim() {
 }
 
 manual_install
+install_system_packages
 install_git_files
 install_misc_sym_links
 install_cpp_env
